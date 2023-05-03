8c7bd8fa1aab75610a409259dd8bcc90ebe0b57b
Jeremy Pogue <jpogue@usc.edu>
2023-05-03 11:42:03 -0700

    correctly parses dict

--- project.py
+++ project.py
@@ -1,11 +1,17 @@
 import asyncio
+import json
 from shazamio import Shazam
 
 
 async def main():
   shazam = Shazam()
-  out = await shazam.recognize_song('dora.ogg')
-  print(out)
+  out = await shazam.recognize_song('kokomo.m4a')
+  pretty = json.dumps(out, indent=4)
+  print(pretty)
+  title = out['track']['title']
+  artist = out['track']['subtitle']
+  print(title)
+  print(artist)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
